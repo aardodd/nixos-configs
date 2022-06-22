4e6e04736b5882d543fe4b2545d969a96e873800
Aaron Dodd <aardodd@users.noreply.github.com>
2022-06-22 17:49:53 +0100

    update

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -11,23 +11,24 @@ if ! findmnt /mnt > /dev/null; then
   parted "/dev/${NIX_INSTALL_BLOCK_DEVICE}" -- mkpart primary linux-swap -${NIX_INSTALL_SWAP_SIZE} 100%
   parted "/dev/${NIX_INSTALL_BLOCK_DEVICE}" -- mkpart ESP fat32 0% 512MiB
   parted "/dev/${NIX_INSTALL_BLOCK_DEVICE}" -- set 3 esp on
-  
+
   echo "Creating filesystems on /dev/${NIX_INSTALL_BLOCK_DEVICE}..."
   mkfs.ext4 -L nixos "/dev/${NIX_INSTALL_BLOCK_DEVICE}1"
   mkswap -L swap "/dev/${NIX_INSTALL_BLOCK_DEVICE}2"
-  mkfs.fat -F 32 -n boot "/dev/${NIX_INSTALL_BLOCK_DEVICE}3"
+  mkfs.fat -F 32 -n EFI "/dev/${NIX_INSTALL_BLOCK_DEVICE}3"
 
   echo "Mounting partitions under mount and enabling swap..."
   mount /dev/disk/by-label/nixos /mnt
   mkdir /mnt/boot
-  mount /dev/disk/by-label/boot /mnt/boot
+  mount /dev/disk/by-label/EFI /mnt/boot
   swapon /dev/disk/by-label/swap
 fi
 
 {
   echo "Building NixOS installation..."
-  if [ -f "./modules/hosts/${NIX_SYSTEM_NAME}/${NIX_INSTALL_NAME}/hardware-configuration.nix" ]; then
-    nixos-generate-config --dir "./modules/hosts/${NIX_SYSTEM_NAME}/${NIX_INSTALL_NAME}/"
+  if [ ! -f "./modules/hosts/${NIX_SYSTEM_NAME}/${NIX_INSTALL_NAME}/hardware-configuration.nix" ]; then
+    nixos-generate-config --root /mnt --dir "./modules/hosts/${NIX_SYSTEM_NAME}/${NIX_INSTALL_NAME}/"
+    git add "./modules/hosts/${NIX_SYSTEM_NAME}/${NIX_INSTALL_NAME}/hardware-configuration.nix"
   fi
 
   nix-shell \
