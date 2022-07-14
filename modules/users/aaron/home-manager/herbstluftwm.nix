51d3cec5ce4cad0eb49d97228e0180103f578450
Aaron Dodd <aardodd@users.noreply.github.com>
2022-07-14 16:25:28 +0100

    Add basic herbstluftwm keybinds

--- modules/users/aaron/home-manager/herbstluftwm.nix
+++ modules/users/aaron/home-manager/herbstluftwm.nix
@@ -8,6 +8,12 @@
       herbstclient set_layout max
       herbstclient detect_monitors
     '';
+    keybinds = {
+      Mod4-Shift-q = "quit";
+      Mod4-Shift-r = "reload";
+      Mod4-Shift-c = "close";
+      Mod4-Return = "spawn kitty";
+    };
     tags = [
         "1" "2" "3" "4" "5" "6" "7" "8" "9" "0"
     ];
