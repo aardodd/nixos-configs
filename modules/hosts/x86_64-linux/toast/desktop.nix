283eba01542a9d0f68bbf0a1b18fbf85668adb7f
Aaron Dodd <aardodd@users.noreply.github.com>
2022-07-15 08:09:01 +0100

    Make awesome the default desktop

--- modules/hosts/x86_64-linux/toast/desktop.nix
+++ modules/hosts/x86_64-linux/toast/desktop.nix
@@ -3,6 +3,6 @@
   services.xserver = {
     enable = true;
     displayManager.lightdm.enable = true;
-    windowManager.herbstluftwm.enable = true;
+    windowManager.awesome.enable = true;
   };
 }
