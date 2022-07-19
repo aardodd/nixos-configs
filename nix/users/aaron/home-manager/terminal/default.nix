c462d8b546f97629bc4780a6b8e4a8df1f5428a1
Aaron Dodd <me@aaronrdodd.com>
2022-07-19 12:34:50 +0000

    Fix terminal defaults

--- nix/users/aaron/home-manager/terminal/default.nix
+++ nix/users/aaron/home-manager/terminal/default.nix
@@ -1,23 +1,23 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
-  config.home.packages = with pkgs; [
+  home.packages = with pkgs; [
     htop
     neovim
     ranger
     ripgrep
   ];
 
-  config.cyber.alacritty = {
+  cyber.alacritty = {
     enable = true;
     configPath = ../../dotfiles/alacritty/alacritty.yml;
   };
 
-  config.home.sessionVariables = {
+  home.sessionVariables = {
     EDITOR = "${pkgs.neovim}/bin/nvim";
     VISUAL = "${pkgs.neovim}/bin/nvim";
   };
 
-  config.programs = {
+  programs = {
     git = {
       enable = true;
       userName = "Aaron Dodd";
